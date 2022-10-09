cfd7a9e80a87bd7f0d26c84013342431416fdce2
kurzickkrozz <115387439+kurzickkrozz@users.noreply.github.com>
2022-10-08 23:48:40 -0500

    Add files via upload

---
+++ widget.js
@@ -0,0 +1,84 @@
+"use strict";
+window.addEventListener('load', () => {
+    for (let widget of document.getElementsByTagName('discord-widget')) {
+        //getting attributes
+        let id = widget.getAttribute('id') ?? '338870081645838337';
+        let width = widget.getAttribute('width') ?? '350px';
+        let height = widget.getAttribute('height') ?? '600px';
+        let footerText = widget.getAttribute('footerText') ?? '';
+        let color = widget.getAttribute('color') ?? '#bb0000';
+        let backgroundColor = widget.getAttribute('backgroundColor') ?? '#0c0c0d';
+        let textColor = widget.getAttribute('textColor') ?? '#fff';
+        let statusColor = widget.getAttribute('statusColor') ?? '#7b0000';
+        if (!id) {
+            console.error(`${widget.outerHTML}, No Discord server ID specified.`);
+        }
+        //header
+        let head = document.createElement('widget-header');
+        let logo = document.createElement('widget-logo') ?? 'https://cdn.discordapp.com/attachments/1028380505923670096/1028380564706840707/pA-ClanTagLogoThicc.png';
+        let count = document.createElement('widget-header-count');
+        head.append(logo, count);
+        //footer
+        let body = document.createElement('widget-body');
+        //footer
+        let footer = document.createElement('widget-footer');
+        let footerInfo = document.createElement('widget-footer-info');
+        let joinButton = document.createElement('widget-button-join');
+        joinButton.addEventListener('click', e => {
+            if (joinButton.getAttribute('href')) {
+                window.open(joinButton.getAttribute('href') || '', joinButton.getAttribute('target') || '', '');
+            }
+        });
+        footerInfo.innerText = footerText;
+        joinButton.innerText = 'Join';
+        footer.append(footerInfo, joinButton);
+        //style
+        widget.innerHTML = '<link rel="stylesheet" href="https://shhh7612.github.io/css/discordWidget.css">';
+        widget.style.height = height;
+        widget.style.width = width;
+        widget.style.setProperty("--color", color);
+        widget.style.setProperty("--bgColor", backgroundColor);
+        widget.style.setProperty("--textColor", textColor);
+        widget.style.setProperty("--buttonColor", `#${LDColor(color.replace('#', ''), -10)}`);
+        widget.style.setProperty("--statusColor", statusColor);
+        //appending head, body and footer to the widget
+        widget.append(head, body, footer);
+        //data
+        fetch(`https://discord.com/api/guilds/${id}/widget.json`).then(data => {
+            data.json().then(data => {
+                //member count
+                count.innerHTML = `<strong>${data.presence_count - 1}</strong> Members Online`;
+                //join button
+                joinButton.setAttribute('href', data.instant_invite);
+                joinButton.setAttribute('target', '_blank');
+                //users
+                data.members.forEach((user) => {
+                    let member = document.createElement('widget-member');
+                    let avatar = document.createElement('widget-member-avatar');
+                    let avatarIMG = document.createElement('img');
+                    let status = document.createElement(`widget-member-status-${user.status}`);
+                    let name = document.createElement('widget-member-name');
+                    let statusText = document.createElement('widget-member-status-text');
+                    avatarIMG.src = user.avatar_url;
+                    status.classList.add('widget-member-status');
+                    name.innerText = user.username;
+                    if (user.game) {
+                        statusText.innerText = user.game.name;
+                    }
+                    avatar.append(avatarIMG, status);
+                    member.append(avatar, name, statusText);
+                    body.append(member);
+                });
+            });
+        });
+    }
+});
+function LDColor(color, percent) {
+    let num = parseInt(color, 16);
+    let amt = Math.round(2.55 * percent);
+    let R = (num >> 16) + amt;
+    let B = (num >> 8 & 0x00FF) + amt;
+    let G = (num & 0x0000FF) + amt;
+    return (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1);
+}
+;
